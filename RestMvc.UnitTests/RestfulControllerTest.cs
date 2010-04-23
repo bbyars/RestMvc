3bbf592c597d17c9dc268ae9a8c2a2bde457c425
unknown <bbyars@.(none)>
2010-04-23 11:40:41 -0600

    Added references

---
+++ RestMvc.UnitTests/RestfulControllerTest.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace RestMvc.UnitTests
+{
+    [TestFixture]
+    public class RestfulControllerTest
+    {
+        
+    }
+}
---
+++ RestMvc/RestfulController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace RestMvc
+{
+    public class RestfulController : Controller
+    {
+        
+    }
+}
