5f98e170117989f3ebd2e64da5ea14ad982b5b74
Scott Muc <scottmuc@gmail.com>
2012-06-13 18:28:21 -0700

    removing app pool spec

--- Install.ps1
+++ Install.ps1
@@ -1,14 +1,12 @@
-param($installPath, $toolsPath, $package, $project)
-
 if ( @(Get-Module | ? { $_.Name -eq "webadministration" }).count -eq 0 ) {
     Import-Module webadministration
 }
 
-function global:Install-IISWebApp {
+function Install-IISWebApp {
     param($label, $path)
 
     # TODO figure out if -Force is the best way to handle the site existing
-    New-Website -Name $label -PhysicalPath $path -ApplicationPool "ASP.NET v4.0" -Port 2112 -Force
+    New-Website -Name $label -PhysicalPath $path -Port 2112 -Force
 
     return $true
 }
