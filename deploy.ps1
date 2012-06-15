fdebd1559ca8f5528a8c3eb1c4ff2d169940152e
Scott Muc <scottmuc@gmail.com>
2012-06-15 15:37:56 -0700

    trying to use implicit credentials for remoting

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
 $hostname = "ec2-174-129-225-146.compute-1.amazonaws.com"
-$username = "Administrator"
-$password = ConvertTo-SecureString "gHg&nci87)i" -AsPlainText -Force
-$credentials = New-Object System.Management.Automation.PSCredential ($username, $password)
 
-
-Invoke-Command -Computer $hostname -Credential $credentials -ScriptBlock { hostname }	
+Invoke-Command -Computer $hostname -ScriptBlock { hostname }	
