9407f6ba4355e520cabb3538e4f8c3dfe26305c3
Tim Long <Tim@tigranetworks.co.uk>
2013-05-17 18:52:30 +0100

    Updated all projects to target .NET 4.5 and ASP.NET MVC 4. Used NuGet for dependencies and enabled automatic package restore. This requires no binaries to be committed to Version Control.

--- RestMvc.FunctionalTests/Routing/EchoControllerTest.cs
+++ RestMvc.FunctionalTests/Routing/EchoControllerTest.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using NUnit.Framework.SyntaxHelpers;
 using RestMvc.FunctionalTests.Assertions;
 
 namespace RestMvc.FunctionalTests.Routing
--- RestMvc.UnitTests/ResourceMapperTest.cs
+++ RestMvc.UnitTests/ResourceMapperTest.cs
@@ -1,7 +1,6 @@
 using System.Web.Mvc;
 using System.Web.Routing;
 using NUnit.Framework;
-using NUnit.Framework.SyntaxHelpers;
 using RestMvc.Attributes;
 using RestMvc.UnitTests.Assertions;
 
